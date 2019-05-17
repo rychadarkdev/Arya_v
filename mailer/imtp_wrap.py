215ce1e077b956749fbbbefdf33b06a154c5685d
rychadarkdev <Rycha7217>
2019-05-17 11:56:13 +0300

    add imap wrap

---
+++ mailer/imtp_wrap.py
@@ -0,0 +1,84 @@
+import imaplib
+import email
+from typing import Optional
+import logging
+
+
+def connect(host: str, port: int) -> Optional[imaplib.IMAP4_SSL]:
+    try:
+        con = imaplib.IMAP4_SSL(host=host, port=port)
+        return con
+    except Exception as error:
+        logging.error(error)
+
+
+def login(mail: str, password: str, con: imaplib.IMAP4_SSL) -> Optional[bool]:
+    try:
+        status, data = con.login(user=mail, password=password)
+        return status == 'OK'
+    except Exception as error:
+        logging.error(error)
+
+
+def load(con: imaplib.IMAP4_SSL) -> Optional[dict]:
+    try:
+        con.select('INBOX')
+        status, data = con.uid('SEARCH', None, 'ALL')
+        if status == 'OK':
+            uids = data[0].split()
+            msgs = dict()
+            for uid in uids:
+                status, data = con.uid('FETCH', uid, '(RFC822)')
+                if status == 'OK':
+                    msg = email.message_from_bytes(data[0][1])
+                    msgs[uid] = msg
+            return msgs
+    except Exception as error:
+        logging.error(error)
+
+
+def copy(uid: str, con: imaplib.IMAP4_SSL) -> Optional[bool]:
+    try:
+        con.create('ARCHIVE')
+        con.select('INBOX')
+        status, data = con.uid('COPY', uid, 'ARCHIVE')
+        return status == 'OK'
+    except Exception as error:
+        logging.error(error)
+
+
+def del_msg(uid: str, con: imaplib.IMAP4_SSL) -> Optional[bool]:
+    try:
+        status, data = con.uid('STORE', uid, '+FLAGS', '(\Deleted)')
+        if status == 'OK':
+            status, data = con.expunge()
+            return status == "OK"
+    except Exception as error:
+        logging.error(error)
+
+
+def save_ta_db(uid: str, msg):
+    print('UID:', uid)
+    print(msg)
+    return True
+
+
+def load_and_save(host: str, port: int, mail: str, password: str) -> Optional[bool]:
+    con = connect(host=host, port=port)
+    if con is None:
+        return
+    if login(mail=mail, password=password, con=con) is not True:
+        return
+    msgs = load(con=con)
+    if not isinstance(msgs, dict):
+        return
+    for uid, msg in msgs.items():
+        if save_ta_db(uid=uid, msg=msg) is not True:
+            continue
+        if copy(uid=uid, con=con) is not True:
+            continue
+        if del_msg(uid=uid, con=con) is not True:
+            continue
+
+
+load_and_save(host='imap.yandex.ru', port=993, mail='hrdhfjdxd@yandex.ru', password='sqAr83cGdrg9ymi')
